4e5b467249ec1a8d066bb2c22cfcb254f69d9288
Clara <clarabsantos02@gmail.com>
2024-05-03 15:05:32 -0300

    cenario1 feito

--- src/cenario1/Pessoa.java
+++ src/cenario1/Pessoa.java
@@ -1,16 +1,25 @@
 package cenario1;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-public class Pessoa {
+public class Pessoa implements List<Pessoa> {
     private String nome;
     private Pessoa conjuge;
     private List<Pessoa> filho;
 
-    public Pessoa(String nome, Pessoa filho) {
+
+    public Pessoa(String nome) {
         this.nome = nome;
         this.filho = new ArrayList<>();
+    }
+
+    public Pessoa(String nome, Pessoa conjuge) {
+        this.nome = nome;
+        this.conjuge = conjuge;
+        this.filho = new ArrayList<>();
+        if (conjuge != null) {
+            conjuge.addFilho(this);
+        }
     }
 
     public String getNome() {
@@ -37,8 +46,12 @@ public class Pessoa {
         this.filho = filho;
     }
 
-    public void addFilho(List<Pessoa> filho) {
-        filho.add((Pessoa) filho);
+    public void addFilho(Pessoa filho) {
+        this.filho.add(filho);
+    }
+
+    public void filhoDe(Pessoa filho){
+
     }
 
     public void addConjuge(Pessoa conjuge) {
@@ -47,22 +60,136 @@ public class Pessoa {
     }
 
     public void printFamilyTree(){
-        System.out.print(nome);
-        if(conjuge != null){
-            System.out.print(" -- Casado(a) com: " + conjuge.getNome());
+        System.out.print(this.nome);
+        if (this.conjuge != null) {
+            System.out.print(" -- Casado(a) com: " + this.conjuge.nome);
         }
         else{
-            System.out.print(" -- Solteiro(a)");
+            System.out.println(" -- Solteiro(a)");
+
         }
-        if(filho != null){
-            for(Pessoa p : filho){
-                System.out.print(" -- Filho(a)" + p.getNome());
+        if (!this.filho.isEmpty()) {
+            System.out.println(" -- Filhos:");
+            for (Pessoa filho : this.filho) {
+                filho.printFamilyTree();
             }
-
-        }
-        else{
-            System.out.print(" Não tem filhos.");
         }
 
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<Pessoa> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean add(Pessoa pessoa) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends Pessoa> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends Pessoa> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Pessoa get(int index) {
+        return null;
+    }
+
+    @Override
+    public Pessoa set(int index, Pessoa element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, Pessoa element) {
+
+    }
+
+    @Override
+    public Pessoa remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<Pessoa> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<Pessoa> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<Pessoa> subList(int fromIndex, int toIndex) {
+        return List.of();
     }
 }
--- src/cenario1/Programa.java
+++ src/cenario1/Programa.java
@@ -2,36 +2,52 @@ package cenario1;
 
 public class Programa {
     public static void main(String[] args) {
-        Pessoa lily = new Pessoa("Lily", null);
-        lily.addConjuge(new Pessoa("Wilhelm", null));
+        Pessoa lily = new Pessoa("Lily");
+        lily.addConjuge(new Pessoa("Wilhelm"));
 
-        Pessoa opa = new Pessoa("Opa", null);
-        opa.addConjuge(new Pessoa("Oma", lily));
+        Pessoa opa = new Pessoa("Opa");
+        opa.addConjuge(new Pessoa("Oma"));
+        lily.addFilho(opa);
 
-        Pessoa reinhold = new Pessoa("Reinhold", opa);
-        Pessoa wilma = new Pessoa("Wilma", opa);
-        Pessoa sigrid = new Pessoa("Sigrid", opa);
+        Pessoa reinhold = new Pessoa("Reinhold");
+        Pessoa wilma = new Pessoa("Wilma");
+        Pessoa sigrid = new Pessoa("Sigrid");
+        opa.addFilho(reinhold);
+        opa.addFilho(wilma);
+        opa.addFilho(sigrid);;
 
-        reinhold.addConjuge(new Pessoa("Sonia", null));
-        Pessoa christian = new Pessoa("Christian", reinhold);
-        Pessoa gabrielle = new Pessoa("Gabrielle", reinhold);
-        Pessoa sabine = new Pessoa("Sabine", reinhold);
+        reinhold.addConjuge(new Pessoa("Sonia"));
 
-        wilma.addConjuge(new Pessoa("Rodolfo", null));
-        Pessoa ricardo = new Pessoa("Ricardo", wilma);
-        Pessoa rodrigo = new Pessoa("Rodrigo", wilma);
+        Pessoa christian = new Pessoa("Christian");
+        christian.addConjuge(new Pessoa("Monica"));
+        Pessoa gabrielle = new Pessoa("Gabrielle");
+        Pessoa sabine = new Pessoa("Sabine");
+        Pessoa oscar = new Pessoa("Oscar");
+        Pessoa lorena = new Pessoa("Lorena");
+        reinhold.addFilho(christian);
+        christian.addFilho(oscar);
+        christian.addFilho(lorena);
+        christian.addFilho(gabrielle);
+        christian.addFilho(sabine);
 
-        ricardo.addConjuge(new Pessoa("Debora", null));
+        wilma.addConjuge(new Pessoa("Rodolfo"));
+        Pessoa ricardo = new Pessoa("Ricardo");
+        ricardo.addConjuge(new Pessoa("Debora"));
+        Pessoa rodrigo = new Pessoa("Rodrigo");
+        wilma.addFilho(ricardo);
+        wilma.addFilho(rodrigo);
 
-        christian.addConjuge(new Pessoa("Monica", null));
-        Pessoa oscar = new Pessoa("Oscar", christian);
-        Pessoa lorena = new Pessoa("Lorena", christian);
 
-        sigrid.addConjuge(new Pessoa("Peter", null));
-        Pessoa martin = new Pessoa("Martin", sigrid);
-        Pessoa thomas = new Pessoa("Thomas", sigrid);
-        Pessoa claudia = new Pessoa("Claudia", sigrid);
-        martin.addConjuge(new Pessoa("Carla", null));
+        sigrid.addConjuge(new Pessoa("Peter"));
+        Pessoa martin = new Pessoa("Martin");
+        martin.addConjuge(new Pessoa("Carla"));
+        Pessoa Nicolas = new Pessoa("Nicolas");
+        Pessoa thomas = new Pessoa("Thomas");
+        Pessoa claudia = new Pessoa("Claudia");
+        sigrid.addFilho(martin);
+        sigrid.addFilho(Nicolas);
+        sigrid.addFilho(thomas);
+        sigrid.addFilho(claudia);
 
         lily.printFamilyTree();
 
